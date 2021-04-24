8f739e070fd8186f9d207dc9e2e05b84d4aab82c
hitiksaini <hitik9045saini@gmail.com>
2021-04-25 01:12:38 +0530

    updated API creds

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
-    define('CONSUMER_KEY', 'JLHF1c9gQ5adfShZU01K5cHZR');
-    define('CONSUMER_SECRET', 'Vra7iqGNsaNQV823RAH0gBFdHUqXCt2usTQr6QbEIECt7Vj903');
+    define('CONSUMER_KEY', 'I7QgKiV1Nkqxjz7CBwbzrCSoD');
+    define('CONSUMER_SECRET', 'e9jjmmlZSVDzHUpDdkzIf5qIljs7D98ByI93BDdynJ8zLi9BDj');
     define('OAUTH_CALLBACK', 'https://rtcampttn.herokuapp.com/callback.php');
 ?>
+<!-- AAAAAAAAAAAAAAAAAAAAACyMOwEAAAAAYUlz9E9NNGdwCjg72tmRRwdGgn8%3DZygg42rykkxpFTvt4jD004rNuUXVv31hdc7d15ms0L1GCK8nR1 -->
